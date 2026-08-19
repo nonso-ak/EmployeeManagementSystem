f49a5be4169fbaa650da6fe7d148a7bdca111414
ak-nonso <akudigwe@sheridancollege.ca>
2026-08-19 09:46:42 -0400

    Login form UI  completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import { Toaster } from "react-hot-toast"
+import { Navigate, Route, Routes } from "react-router-dom"
+import PrintPayslip from "./pages/PrintPayslip"
+import Layout from "./pages/Layout"
+import Attendance from "./pages/Attendance"
+import Dashboard from "./pages/Dashboard"
+import Employees from "./pages/Employees"
+import Payslips from "./pages/Payslips"
+import Leave from "./pages/Leave"
+import Settings from "./pages/Settings"
+import LoginLanding from "./pages/LoginLanding"
+import LoginForm from "./components/LoginForm"
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element= { <LoginLanding/> }/>
+
+        <Route path="/login/admin" element= { <LoginForm role="admin" title="Admin Portal" subtitle="Sign in to manage the organization"/> }/>
+
+        <Route path="/login/employee" element= { <LoginForm role="employee" title="Employee Portal" subtitle="Sign in to access your account"/> }/>
+
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/employees" element={<Employees />}/>
+          <Route path="/attendance" element={<Attendance />}/>
+          <Route path="/leave" element={<Leave />}/>
+          <Route path="/payslips" element={<Payslips />}/>
+          <Route path="/settings" element={<Settings />}/>
+        </Route>
+        <Route path="/print/payslips/:id" element= { <PrintPayslip/> }/>
+
+        <Route path="*" element={<Navigate to="/dashboard" replace/>}/>
+      </Routes>
+    </>
   )
 }
 
